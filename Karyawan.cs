9ee24bddc0bcf74747fc233b860428280eb1f9a8
vickyreza <vicky.fa@students.amikom.ac.id>
2020-04-15 15:30:46 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2904
+{
+    public class Karyawan
+    {
+        public Karyawan(string name, int nik, int gajibulanan)
+        {
+            Name = name;
+            Nik = nik;
+            GajiBulanan = gajibulanan;
+
+
+        }
+        public string Name { get; set; }
+        public int Nik { get; set; }
+        public int GajiBulanan { get; set; }
+
+
+        public void GetNameAndNikAndGajiBulanan()
+        {
+            Console.WriteLine("Nama : {0} Nik : {1} Gaji Bulanan : {2}", Name, Nik, GajiBulanan);
+        }
+
+        public void GajiNaik()
+        {
+            Console.WriteLine("yee gaji naik 10%");
+            Console.WriteLine("Nama : {0} Nik : {1} Gaji Bulanan : {2}", Name, Nik, GajiBulanan+(GajiBulanan*10 / 100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResponsiPemrograman2904
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan("reja", 19112904, 23000);
+            karyawan.GetNameAndNikAndGajiBulanan();
+            karyawan.GajiNaik();
+            Karyawan karyawan2 = new Karyawan("vicky", 11111111, 23000);
+            karyawan2.GetNameAndNikAndGajiBulanan();
+            karyawan2.GajiNaik();
+        }
+    }
+}
